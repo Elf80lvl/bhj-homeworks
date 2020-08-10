7b0acd7903530b330f5de86074a48f631478d0c8
Richard Winter <a.blake.mail@gmail.com>
2020-08-10 11:50:56 +0300

    complete v1.7

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -2,7 +2,6 @@ const minusButton = Array.from(document.getElementsByClassName('product__quantit
 const plusButton = Array.from(document.getElementsByClassName('product__quantity-control_inc'));
 const buttonsAdd = Array.from(document.getElementsByClassName('product__add'));
 const cart = document.getElementsByClassName('cart__products')[0];
-
 let id, quantity, imgsrc, products;
 
 //При нажатии на кнопки увеличения/уменьшения количества товаров, число должно также меняться. Минимальное число товаров - 1 (отрицательные значения не допускаются)
@@ -14,11 +13,13 @@ plusButton.forEach((el) => {
   el.addEventListener('click', plusOne);
 });
 
+
 function minusOne() {
   if (this.parentNode.querySelector('.product__quantity-value').textContent > 0){
     this.parentNode.querySelector('.product__quantity-value').textContent -= 1;
   }
 }
+
 
 function plusOne() {
   this.parentNode.querySelector('.product__quantity-value').textContent  = parseInt(this.parentNode.querySelector('.product__quantity-value').textContent) + 1;
@@ -30,25 +31,21 @@ buttonsAdd.forEach((el) => {
   el.addEventListener('click', buttonClick);
 });
 
+
 function buttonClick() {
   id = this.closest('.product').dataset.id;
   quantity = parseInt(this.closest('.product__quantity').querySelector('.product__quantity-value').textContent);
   imgsrc = this.closest('.product').querySelector('.product__image').getAttribute('src');
-
   products = Array.from(document.querySelectorAll('.cart__product'));
     products.forEach((el) => {
       if (el.dataset.id == id) {
         //добавляем число к товару который уже в корзине
         el.querySelector('.cart__product-count').textContent = parseInt(el.querySelector('.cart__product-count').textContent) + quantity;
-        console.log(el)
         el.parentNode.children[el.parentNode.children - 1].remove();
       }
   })
-
 if (quantity > 0) addTheProductToCart();
-products = Array.from(document.querySelectorAll('.cart__product'));
-}//enf of function
-
+}
 
 
 function addTheProductToCart() {
