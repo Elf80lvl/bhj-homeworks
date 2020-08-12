f39d31e68ed3a854f424d1d5bd375c7dab36619e
Richard Winter <a.blake.mail@gmail.com>
2020-08-12 13:49:23 +0300

    complete v1.1

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -22,18 +22,5 @@ function submitForm(){
   formData.append('file', file);
   //request
   xhr.send(formData);
-
   console.log(file);
-
 }
-
-
-// form.addEventListener('submit', e => {    
-//   e.preventDefault();
-//   let formData = new FormData(form);    
-//   let xhr = new XMLHttpRequest();    
-//   xhr.upload.addEventListener('progress', e => progress.value = e.loaded / e.total);    
-//   xhr.open('POST', 'https://netology-slow-rest.herokuapp.com/upload.php');    
-//   xhr.send(formData); 
-
-// });
